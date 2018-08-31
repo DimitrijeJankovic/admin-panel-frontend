15d1ca7e9dbc2c892bd7094aa8b35d10d767ff59
dimitrije.jankovic <dimitrije.jankovic@kelcix.com>
2018-08-31 14:58:21 +0200

    default rout /admin/login

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,7 +7,7 @@ import { AuthtRouteActivator } from './user/auth-route-activation.service';
 export const appRoutes:Routes = [
     { path: 'user', loadChildren: () => UserModule },
     { path: 'admin', loadChildren: () => AdminModule },
-    { path: '', redirectTo: '/user/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
     { path: '404', component: Error404Component }
 
 ]
